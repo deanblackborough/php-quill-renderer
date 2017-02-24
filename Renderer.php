3e7884f38cdd993d73a60e8902ad2c6720285f57
Dean Blackborough <dean@g3d-development.com>
2017-02-24 16:43:36 +0000

    Structure
    - Initial class structure

--- Renderer.php
+++ Renderer.php
@@ -5,6 +5,10 @@ namespace DBlackborough\Quill;
 /**
  * Quill renderer, converts quill delta inserts into html
  *
+ * @todo Validate options
+ * @todo Tests for each attribute
+ * @todo Tests for each container supported
+ *
  * @author Dean Blackborough <dean@g3d-development.com>
  * @copyright Dean Blackborough
  * @license https://github.com/deanblackborough/php-quill-renderer/blob/master/LICENSE
@@ -16,24 +20,75 @@ class Renderer
      *
      * @var array
      */
-    private $inserts;
+    private $deltas;
+
+    /**
+     * Valid inserts json array
+     *
+     * @param boolean
+     */
+    private $json_valid = false;
+
+    /**
+     * Options data array
+     *
+     * @param array
+     */
+    private $options = array();
 
     /**
      * Renderer constructor.
      *
-     * @return \DBlackborough\Quill\Renderer
+     * @param array @options Options data array, if empty default options are used
      */
-    public function __construct()
+    public function __construct(array $options = array())
     {
-        return $this;
+        if (count($options) === 0) {
+            $options = $this->defaultOptions();
+        }
+
+        $this->setOptions($options);
     }
 
     /**
-     * @return string
+     * Default options
+     *
+     * @return array
      */
-    public function toHtml()
+    private function defaultOptions()
     {
+        return array(
+            'attributes' => array(
+                'bold',
+                'italic',
+                'underline',
+                'strike'
+            ),
+            'containerTag' => 'p'
+        );
+    }
 
+    /**
+     * Default options
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Set the options for the renderer
+     *
+     * @param array $options
+     * @return \DBlackborough\Quill\Renderer
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+
+        return $this;
     }
 
     /**
@@ -42,6 +97,19 @@ class Renderer
      * @return boolean
      */
     public function load($inserts)
+    {
+        if ($this->deltas = json_decode($inserts, true) !== null) {
+            $this->json_valid = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function toHtml()
     {
 
     }
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'Renderer.php';
+
+$deltas = '{"ops":[{"insert":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur nibh tempor augue lobortis, nec eleifend velit venenatis. Nullam fringilla dui eget lectus mattis tincidunt. \nDonec sollicitudin, lacus sed luctus ultricies, "},{"attributes":{"strike":true,"italic":true},"insert":"quam sapien "},{"attributes":{"strike":true},"insert":"sollicitudin"},{"insert":" quam, nec auctor eros felis elementum quam. Fusce vel mollis enim. \n\n"},{"attributes":{"bold":true},"insert":"Sed ac augue tincidunt,"},{"insert":" cursus urna a, tempus ipsum. Donec pretium fermentum erat a "},{"attributes":{"underline":true},"insert":"elementum"},{"insert":". In est odio, mattis sed dignissim sed, porta ac nisl. Nunc et tellus imperdiet turpis placerat tristique nec quis justo. Aenean nisi libero, auctor a laoreet sed, fermentum vel massa.\n\nEtiam ultricies leo eget purus tempor dapibus. Integer ac sapien eros. Suspendisse convallis ex \n"}]}';
+
+$renderer = new \DBlackborough\Quill\Renderer();
+
+if ($renderer->load($deltas) === true) {
+
+}
