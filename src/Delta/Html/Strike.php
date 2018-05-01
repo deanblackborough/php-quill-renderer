2e0c952db33402b4b0ff84668ace401a52898678
Dean Blackborough <dean@g3d-development.com>
2018-05-01 23:34:10 +0100

    Typography
    * Added Delta objects for basic typography deltas, all Typography tests pass

---
+++ src/Delta/Html/Strike.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBlackborough\Quill\Delta\Html;
+
+/**
+ * Default delta class for inserts with the 'strike' attribute
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough
+ * @license https://github.com/deanblackborough/php-quill-renderer/blob/master/LICENSE
+ */
+class Strike extends Delta
+{
+    /**
+     * Set the initial properties for the delta
+     *
+     * @param string $insert
+     * @param array $attributes
+     */
+    public function __construct(string $insert, array $attributes = [])
+    {
+        $this->tag = 's';
+
+        $this->insert = $insert;
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Return the display type for the resultant HTML created by the delta, either inline or block
+     *
+     * @return string
+     */
+    public function displayType() : string
+    {
+        return parent::DISPLAY_INLINE;
+    }
+
+    /**
+     * Render the HTML for the specific Delta type
+     *
+     * @return string
+     */
+    public function render(): string
+    {
+        return "<{$this->tag}>{$this->insert}</{$this->tag}>";
+    }
+}
---
+++ src/Delta/Html/SubScript.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBlackborough\Quill\Delta\Html;
+
+/**
+ * Default delta class for inserts with the 'script' attribute set to 'sub'
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough
+ * @license https://github.com/deanblackborough/php-quill-renderer/blob/master/LICENSE
+ */
+class SubScript extends Delta
+{
+    /**
+     * Set the initial properties for the delta
+     *
+     * @param string $insert
+     * @param array $attributes
+     */
+    public function __construct(string $insert, array $attributes = [])
+    {
+        $this->tag = 'sub';
+
+        $this->insert = $insert;
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Return the display type for the resultant HTML created by the delta, either inline or block
+     *
+     * @return string
+     */
+    public function displayType() : string
+    {
+        return parent::DISPLAY_INLINE;
+    }
+
+    /**
+     * Render the HTML for the specific Delta type
+     *
+     * @return string
+     */
+    public function render(): string
+    {
+        return "<{$this->tag}>{$this->insert}</{$this->tag}>";
+    }
+}
