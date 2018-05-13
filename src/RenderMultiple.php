4d54a2f68b4fc4829c83284b1bf4777221030a56
Dean Blackborough <dean@g3d-development.com>
2018-05-13 13:37:12 +0100

    Render multiple
    * Initial empty class for multiple parsing support.

---
+++ src/RenderMultiple.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace DBlackborough\Quill;
+
+/**
+ * Parse multiple Quill generated deltas strings into the requested format, you can return
+ * then rendered strings in whatever order you want, just provide the relevant index
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough
+ * @license https://github.com/deanblackborough/php-quill-renderer/blob/master/LICENSE
+ */
+class RenderMultiple
+{
+    
+}
