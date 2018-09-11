3ce2429c5fbcf4ce2beafe566ab18011930dd25d
Dean Blackborough <dean@g3d-development.com>
2018-09-11 14:18:36 +0100

    New line insert bug
    * Added a test for bug report 101.
    * Updated a few existing tests to remove excess new lines.
    * Updated composer dependencies.
    * Updated logic, no longer ignore inserts which only contain a new line.

--- Tests/Attributes/Html/VideoTest.php
+++ Tests/Attributes/Html/VideoTest.php
@@ -11,7 +11,7 @@ use DBlackborough\Quill\Render as QuillRender;
  */
 final class VideoTest extends \PHPUnit\Framework\TestCase
 {
-    private $delta_video = '{"ops":[{"insert":{"video":"https://video.url"}},{"insert":"\n"}]}';
+    private $delta_video = '{"ops":[{"insert":{"video":"https://video.url"}}]}';
 
     private $expected_video = '<p><iframe class="ql-video" frameborder="0" allowfullscreen="true" src="https://video.url"></iframe></p>';
 
