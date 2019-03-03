8ff1115c211bb9666c2d0f711c558e5926b27858
Dean Blackborough <dean@g3d-development.com>
2019-03-03 15:55:56 +0000

    Added failing test
    * Added failing test for #109

--- Tests/Api/BugTest.php
+++ Tests/Api/BugTest.php
@@ -55,6 +55,130 @@ final class BugTest extends \PHPUnit\Framework\TestCase
             }
         ]
     }';
+    private $delta_bug_109_list_outout_incorrect = '
+    {
+        "ops": [
+            {
+                "insert": "Headline 1"
+            },
+            {
+                "attributes": {
+                    "header": 1
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Some text. "
+            },
+            {
+                "attributes": {
+                    "bold": true
+                },
+                "insert": "Bold Text"
+            },
+            {
+              "insert": ". "
+            },
+            {
+                "attributes": {
+                    "italic": true
+                },
+                "insert": "Italic Text"
+            },
+            {
+                "insert": ". "
+            },
+            {
+                "attributes": {
+                    "italic": true,
+                    "bold": true
+                },
+                "insert": "Bold and italic Text"
+            },
+            {
+                "insert": ". Here is a "
+            },
+            {
+                "attributes": {
+                    "link": "https://scrumpy.io"
+                },
+                "insert": "Link"
+            },
+            {
+                "insert": ". \nHeadline 2"
+            },
+            {
+                "attributes": {
+                    "header": 2
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "ordered list item"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "ordered list item"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "ordered list item"
+            },
+            {
+                "attributes": {
+                    "list": "ordered"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "unordered list item"
+            },
+            {
+                "attributes": {
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "unordered list item with "
+            },
+            {
+                "attributes": {
+                    "bold": true,
+                    "link": "https://scrumpy.io"
+              },
+              "insert": "link"
+            },
+            {
+                "attributes": {
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "unordered list item"
+            },
+            {
+                "attributes": {
+                    "list": "bullet"
+                },
+                "insert": "\n"
+            },
+            {
+                "insert": "Some Text.\n"
+            }
+        ]
+    }';
 
     private $expected_bug_external_3 = '<p>Lorem ipsum
 <br />
@@ -66,6 +190,7 @@ Lorem ipsum
 </p>';
     private $expected_bug_108_link_within_header = '<h2>This is a <a href="https://link.com">header</a>, it has a link within it.</h2>';
     private $expected_bug_108_link_end_of_header = '<h2>This is a header, with a link at the <a href="https://link.com">end</a></h2>';
+    private $expected_bug_109_list_outout_incorrect = '';
 
     /**
      * Newlines still proving to be an issue
@@ -139,6 +264,31 @@ Lorem ipsum
             $this->expected_bug_108_link_end_of_header,
             trim($result),
             __METHOD__ . ' link at end of header'
+        );
+    }
+
+    /**
+     * Link at the end of a header
+     * Bug report https://github.com/deanblackborough/php-quill-renderer/issues/108
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function testIncorrectListOutput()
+    {
+        $result = null;
+
+        try {
+            $quill = new QuillRender($this->delta_bug_109_list_outout_incorrect);
+            $result = $quill->render();
+        } catch (\Exception $e) {
+            $this->fail(__METHOD__ . 'failure, ' . $e->getMessage());
+        }
+
+        $this->assertEquals(
+            $this->expected_bug_109_list_outout_incorrect,
+            trim($result),
+            __METHOD__ . ' list output incorrect'
         );
     }
 }
